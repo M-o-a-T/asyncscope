1bed34513ad07404fd25172ac28ed5689045c775
Matthias Urlichs <matthias@urlichs.de>
2020-07-16 12:38:09 +0200

    Basic setup

---
+++ Makefile
@@ -0,0 +1,11 @@
+#!/usr/bin/make -f
+
+ifneq ($(wildcard /usr/share/sourcemgr/make/py),)
+include /usr/share/sourcemgr/make/py
+# availabe via http://github.com/smurfix/sourcemgr
+
+else
+%:
+		@echo "Please use 'python setup.py'."
+		@exit 1
+endif
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+LONG_DESC = open("README.rst", encoding="utf-8").read()
+
+setup(
+    name="asyncscope",
+    use_scm_version={"version_scheme": "guess-next-dev", "local_scheme": "dirty-tag"},
+    setup_requires=["setuptools_scm"],
+    description="Task scopes for AnyIO",
+    url="http://github.com/M-o-a-T/asyncscope",
+    long_description=LONG_DESC,
+    author="Matthias Urlichs",
+    author_email="matthias@urlichs.de",
+    license="GPLv3 or later",
+    packages=find_packages(),
+    install_requires=["anyio >= 1.4", "trio >= 0.16"],
+    keywords=["async"],
+    python_requires=">=3.6",
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Framework :: Trio",
+        "Framework :: asyncio",
+        "Framework :: AnyIO",
+    ],
+)
