e7e09904160325a919860540dd36c8e96bb2bfdf
Matthias Urlichs <matthias@urlichs.de>
2022-12-17 12:16:01 +0100

    tests: set up logging

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,34 @@
+import logging
+_cfg={  # a magic incantation
+    "version": 1,
+    # "loggers": {"asyncserf": {"level": "INFO"}, "xknx.raw_socket": {"level": "INFO"}},
+    "root": {"handlers": ["stderr"], "level": "INFO"},
+    "handlers": {
+        #           "logfile": {
+        #               "class": "logging.FileHandler",
+        #               "filename": "test.log",
+        #               "level": "DEBUG",
+        #               "formatter": "std",
+        #           },
+        "stderr": {
+            "class": "logging.StreamHandler",
+            "level": "DEBUG",
+            "formatter": "std",
+            "stream": "ext://sys.stderr",
+        }
+    },
+    "formatters": {
+        "std": {
+            "class": "moat.util.TimeOnlyFormatter",
+            "format": "%(asctime)s %(levelname)s:%(name)s:%(message)s",
+        }
+    },
+    "disable_existing_loggers": False,
+}
+from logging.config import dictConfig as _config
+_config(_cfg)
+
+
 class Stepper:
     def __init__(self):
         self.value = 0
